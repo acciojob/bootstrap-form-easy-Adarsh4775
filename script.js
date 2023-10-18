a0378ffbfa35814d43c632c1c5bddbe6d1cebf16
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-18 13:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+function bootForm(){
+	const firstName = document.getElementById("first-name-input");
+	const lastName = document.getElementById("last-name-input");
+    const email = document.getElementById("email-input");
+	const college = document.getElementById("college-input");
+	const graduation = document.getElementById("graduation-input");
+	const rollNo = document.getElementById("roll-no-input");
+	const checkBox = document.getElementById("conditions-checkbox");
+
+
+}
